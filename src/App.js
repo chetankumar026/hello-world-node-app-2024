71c760d529b1918697425f603212835226e6cdec
YameenGithub <yameensayed11@gmail.com>
2024-08-17 17:40:40 +0530

    added line in App.JS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      <h2>Mai iss Duniya ka PAPA hu</h2>
+      <h2>I AM PRO in DevOps</h2>
+      <p>Mai iss Duniya ka PAPA hu.....HAHAHAHAHAHA</p>
     </div>
   );
 }
